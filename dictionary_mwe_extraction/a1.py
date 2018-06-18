9cf88b71023a520f68ca8434d8a284b0ea6492eb
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-06-18 11:30:48 +0100

    Deal in entry objs instead of strs

--- dictionary_mwe_extraction/a1.py
+++ dictionary_mwe_extraction/a1.py
@@ -1,5 +1,6 @@
 # xmlns="urn:NEIDTRANS" xmlns:d="urn:NEIDTRANS" xmlns:md="urn:DPS2-metadata"
 
+import json
 import sys
 
 import xml.etree.ElementTree as ET
@@ -15,24 +16,26 @@ def get_collocations_from_entry(entry):
     #print(ET.dump(entry))
     hwd_obj = entry.find('.//{urn:NEIDTRANS}HWD')
     hwd = None
+    a = {'hwd': None}
     if hwd_obj is not None:
-        hwd = hwd_obj.text.strip()
-    return [hwd]
+        a['hwd'] = hwd_obj.text.strip()
+
+    return [a]
 
 def main(xml_path):
     ET.register_namespace('', 'urn:NEDITRANS')
     ET.register_namespace('d', 'urn:NEIDTRANS')
     ET.register_namespace('md', 'urn:DPS2-metadata')
     e = ET.parse(xml_path).getroot()
-    collocations = []
+    entries = []
     n = 0
     for entry in e.findall('{urn:NEIDTRANS}Entry'):
-        collocations.extend(get_collocations_from_entry(entry))
+        entries.append(get_collocations_from_entry(entry))
         n += 1
         if n > 1000:
             break
-    print('collocations:')
-    print('\n'.join(collocations))
+    print('entries:')
+    print('\n'.join(json.dumps(e) for e in entries))
 
 if __name__ == '__main__':
     path = ' '.join(sys.argv[1:])
