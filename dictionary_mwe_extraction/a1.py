744cbad379324a24d433624d32393ebc47bf64a0
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-06-18 13:29:40 +0100

    Add conjblks

--- dictionary_mwe_extraction/a1.py
+++ dictionary_mwe_extraction/a1.py
@@ -57,6 +57,9 @@ def get_collocations_from_entry(entry):
     for pos in entry.findall('.//{urn:NEIDTRANS}InterjBlk/{urn:NEIDTRANS}FwkSenCnt'):
         d = get_data_from_pos(pos)
         a['entries'].append(d)
+    for pos in entry.findall('.//{urn:NEIDTRANS}ConjBlk/{urn:NEIDTRANS}FwkSenCnt'):
+        d = get_data_from_pos(pos)
+        a['entries'].append(d)
     for pos in entry.findall('.//{urn:NEIDTRANS}PhrBlk/{urn:NEIDTRANS}PhrCnt/{urn:NEIDTRANS}FwkSenCnt'):
         d = get_data_from_pos(pos)
         a['entries'].append(d)
