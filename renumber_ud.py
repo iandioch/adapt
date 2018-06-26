55149cb4d145d6d6034ef858907c0196773bcb74
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-06-26 15:37:43 +0100

    Reorder renumber script to fix empty line crash

--- renumber_ud.py
+++ renumber_ud.py
@@ -6,16 +6,17 @@ def end_of_sentence(lines):
     for i, line in enumerate(lines):
         p = line.strip().split('\t')
         print('\t'.join([str(i+1)] + p[1:]))
+    print()
 
 def main():
     lines = []
     for line in sys.stdin.readlines():
         line = line.strip()
-        if line[0] == '#':
-            print(line)
-        elif len(line) < 2:
+        if len(line) < 2:
             end_of_sentence(lines)
             lines = []
+        elif line[0] == '#':
+            print(line)
         else:
             lines.append(line)
     end_of_sentence(lines)
