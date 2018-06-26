cfcebaff4a8327e1f10a4e91818f4e60acf4bebe
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-06-26 16:43:46 +0100

    output sentence id if not present in renumber_ud.py

--- renumber_ud.py
+++ renumber_ud.py
@@ -1,8 +1,10 @@
 import sys
 
-def end_of_sentence(lines):
+def end_of_sentence(lines, outputted_id, curr_id):
     if len(lines) == 0:
         return
+    if not outputted_id:
+        print('# sent_id = {}'.format(curr_id))
     for i, line in enumerate(lines):
         p = line.strip().split('\t')
         print('\t'.join([str(i+1)] + p[1:]))
@@ -10,12 +12,19 @@ def end_of_sentence(lines):
 
 def main():
     lines = []
+    outputted_id = False
+    curr_id = 1
     for line in sys.stdin.readlines():
         line = line.strip()
         if len(line) < 2:
-            end_of_sentence(lines)
+            end_of_sentence(lines, outputted_id, curr_id)
             lines = []
+
+            outputted_id = False
+            curr_id += 1
         elif line[0] == '#':
+            if 'sent_id' in line:
+                outputted_id = True
             print(line)
         else:
             lines.append(line)
