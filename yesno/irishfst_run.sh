622adb17b617df6187b7855207af3054d752b005
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-07-16 10:18:51 +0100

    Add irishfst run script

---
+++ yesno/irishfst_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# loop through all files in directory and process all 
+# 1. tokenize input text
+# 2. flookup morphological analysis
+# 3. convert to required CG i/p format
+# 4. vislcg3  disambiguation
+# elaine oct 2012,  Noah (iandioch11@gmail.com) June 2018
+
+IRISHFST=~/work/irishfst
+cat - | tr -sc "[0-9a-zA-ZáéíóúÁÉÍÓÚ.?,]" "[\n*]" | flookup -a $IRISHFST/bin/lexguess.fst | perl $IRISHFST/dis/lookup2cg3.prl | vislcg3 -g $IRISHFST/dis/gael-dis.rle > irishfst.out
+echo "done"
+
+
