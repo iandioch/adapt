389b87c68ef925ea4833199799e010c022ac1164
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-07-16 15:19:14 +0100

    hide irishfst errors

--- yesno/process.py
+++ yesno/process.py
@@ -10,17 +10,23 @@ import pos2conll
 
 # Path to script to tokenise and run POS tagger.
 PATH_TO_IRISHFST_SCRIPT = './irishfst_run.sh'
+OUTPUT_IRISHFST_ERRORS = False
 
 MALTPARSER_JAR_PATH = '/home/noah/work/misc_tools/maltparser/maltparser-1.9.1/maltparser-1.9.1.jar'
 MALTPARSER_CONFIG_PATH = '/home/noah/work/misc_tools/maltparser/maltparser-1.9.1/'
 MALTPARSER_CONFIG_NAME = 'IrishTreebankYesNo'
 
 
+
 def run_irishfst(line):
     command = ['sh', PATH_TO_IRISHFST_SCRIPT]
     proc = subprocess.Popen(command, stdin=subprocess.PIPE,
-                            stdout=subprocess.PIPE, encoding='utf-8')
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, encoding='utf-8')
     output, error = proc.communicate(input=line)
+    if error is not None and OUTPUT_IRISHFST_ERRORS:
+        print('irishfst error:')
+        print(error)
     return output
 
 
