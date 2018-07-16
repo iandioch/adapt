d591c60d9c7401433cace7a33d6ad3c477de64ff
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-07-16 12:38:44 +0100

    pipe irishfst output through conll converter

--- yesno/process.py
+++ yesno/process.py
@@ -2,8 +2,11 @@
 Expects as input one one line the Gaeilge question.
 '''
 
+import io
 import subprocess
 import sys
+
+import pos2conll
 
 # Path to script to tokenise and run POS tagger.
 PATH_TO_IRISHFST_SCRIPT = './irishfst_run.sh'
@@ -12,8 +15,12 @@ def run_irishfst(line):
     command = ['sh', PATH_TO_IRISHFST_SCRIPT]
     proc = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8')
     output, error = proc.communicate(input=line)
-    print(output)
-    print(error)
+    return output
+
+def irishfst_output_to_conll(sents):
+    inf = io.StringIO(sents)
+    outf = sys.stdout
+    pos2conll.convert(inf, outf)
 
 def process(line):
     # tokenise line
@@ -21,6 +28,7 @@ def process(line):
     # convert to conllu
     # extract useful parts
     pos_tagged = run_irishfst(line)
+    conll = irishfst_output_to_conll(pos_tagged)
 
 def main():
     for line in sys.stdin:
