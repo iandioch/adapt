d8a0d41e730fc1c9b3e4d0ba3599506c392cb7d0
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-07-16 14:48:30 +0100

    Put conll through Maltparser to get dependencies

--- yesno/process.py
+++ yesno/process.py
@@ -11,6 +11,10 @@ import pos2conll
 # Path to script to tokenise and run POS tagger.
 PATH_TO_IRISHFST_SCRIPT = './irishfst_run.sh'
 
+MALTPARSER_JAR_PATH = '/home/noah/work/misc_tools/maltparser/maltparser-1.9.1/maltparser-1.9.1.jar'
+MALTPARSER_CONFIG_PATH = '/home/noah/work/misc_tools/maltparser/maltparser-1.9.1/'
+MALTPARSER_CONFIG_NAME = 'IrishTreebankYesNo'
+
 def run_irishfst(line):
     command = ['sh', PATH_TO_IRISHFST_SCRIPT]
     proc = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8')
@@ -24,6 +28,14 @@ def irishfst_output_to_conll(sents):
         outf.seek(0)
         return outf.read()
 
+def parse_dependencies(sents):
+    command = ['java', '-jar', MALTPARSER_JAR_PATH, '-c', MALTPARSER_CONFIG_NAME, '-m', 'parse']
+    proc = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8', cwd=MALTPARSER_CONFIG_PATH)
+    output, error = proc.communicate(input=sents)
+    print(output)
+    print('error:')
+    print(error)
+
 def process(line):
     # tokenise line
     # run POS tagger on it
@@ -31,7 +43,7 @@ def process(line):
     # extract useful parts
     pos_tagged = run_irishfst(line)
     conll = irishfst_output_to_conll(pos_tagged)
-    print(conll)
+    deps = parse_dependencies(conll)
 
 def main():
     for line in sys.stdin:
