678343cabbfcc7f0b3ac05851a6e9c96b62f3fc1
Noah Ó Donnaile# <iandioch11@gmail.com>
2018-07-16 10:13:27 +0100

    Add yesno/process.py

---
+++ yesno/process.py
@@ -0,0 +1,18 @@
+'''
+Expects as input one one line the Gaeilge question.
+'''
+
+import sys
+
+def process(line):
+    # tokenise line
+    # run POS tagger on it
+    # convert to conllu
+    # extract useful parts
+
+def main():
+    for line in sys.stdin:
+               process(line)
+
+if __name__ == '__main__':
+    main()
